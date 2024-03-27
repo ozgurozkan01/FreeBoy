677817b60b043591c76fdc08e5a52b572751e501
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-03-27 16:15:20 +0300

    create template

--- src/gameboy/GameBoy.cpp
+++ src/gameboy/GameBoy.cpp
@@ -3,3 +3,33 @@
 //
 
 #include "GameBoy.h"
+#include <cstdio>
+#include "../../src/graphics/Graphic.h"
+
+GameBoy::GameBoy() :
+    graphic(new Graphic(160, 144, "GameBoy-Emulator")),
+    emulatorState(EmulatorState::RUNNING)
+{
+}
+
+bool GameBoy::init()
+{
+    if (!graphic->init())
+    {
+        printf("ERROR : Graphics could not be loaded!");
+        return false;
+    }
+
+/*    if (!cartridge->loadCartridge((char*)""))
+    {
+        printf("ERROR : Cartridge could not be loaded!");
+        return false;
+    }*/
+
+    return true;
+}
+
+void GameBoy::run()
+{
+    graphic->run();
+}
--- src/gameboy/GameBoy.h
+++ src/gameboy/GameBoy.h
@@ -5,9 +5,25 @@
 #ifndef GAMEBOY_EMULATOR_SDL2_GAMEBOY_H
 #define GAMEBOY_EMULATOR_SDL2_GAMEBOY_H
 
+#include <cstdint>
+
+enum class EmulatorState : uint8_t
+{
+    PAUSE,
+    RUNNING
+};
+
+class Graphic;
 
 class GameBoy {
+public:
+    GameBoy();
+    bool init();
+    void run();
+private:
+    EmulatorState emulatorState;
 
+    Graphic* graphic;
 };
 
 
