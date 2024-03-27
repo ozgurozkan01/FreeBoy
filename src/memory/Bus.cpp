d8a61fca68834389976596adb1029fff7442cff0
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-03-27 16:27:30 +0300

    create Bus class

---
+++ src/memory/Bus.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ozgur on 27/03/2024.
+//
+
+#include "Bus.h"
---
+++ src/memory/Bus.h
@@ -0,0 +1,14 @@
+//
+// Created by ozgur on 27/03/2024.
+//
+
+#ifndef GAMEBOY_EMULATOR_SDL2_BUS_H
+#define GAMEBOY_EMULATOR_SDL2_BUS_H
+
+
+class Bus {
+
+};
+
+
+#endif //GAMEBOY_EMULATOR_SDL2_BUS_H
