cf3bffb76e4555d890d81555f448850dd0d62f58
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-03-28 10:57:52 +0300

    create initial file

--- src/memory/Bus.cpp
+++ src/memory/Bus.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "Bus.h"
+
+Bus::Bus() {
+
+}
+// Get data from the memory
+uint8_t Bus::busRead(uint16_t address) {
+    return 0;
+}
+// Put data to the memory
+void Bus::busWrite(uint16_t address, uint8_t value) {
+
+}
--- src/memory/Bus.h
+++ src/memory/Bus.h
@@ -5,9 +5,14 @@
 #ifndef GAMEBOY_EMULATOR_SDL2_BUS_H
 #define GAMEBOY_EMULATOR_SDL2_BUS_H
 
+#include <cstdint>
 
 class Bus {
-
+public:
+    Bus();
+    uint8_t busRead(uint16_t address);
+    void busWrite(uint16_t address, uint8_t value);
+private:
 };
 
 
