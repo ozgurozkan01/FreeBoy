e295c0cf5273626ad9bed2d36bfcd81419f70d72
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-04-12 16:25:29 +0300

    add bitwise operation methods

--- freeboy/include/util/Utility.h
+++ freeboy/include/util/Utility.h
@@ -5,4 +5,15 @@
 #ifndef FREEBOY_UTILITY_H
 #define FREEBOY_UTILITY_H
 
+#include <cstdint>
+#include <iostream>
+
+namespace gameboy
+{
+    static void bitSet(int& _variable, int _shifting) { _variable |= 1 << _shifting; }
+    static void bitReset(int& _variable, int _shifting) { _variable &= ~(1 << _shifting); }
+    static uint8_t bitGet(int& _variable, int _shifting) { return _variable &= 1 << _shifting; }
+    static void noImplementation() { std::cout << "NOT YET IMPLEMENTED!!\n"; }
+}
+
 #endif //FREEBOY_UTILITY_H
