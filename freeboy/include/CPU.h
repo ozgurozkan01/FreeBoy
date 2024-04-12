1535cf7b57c26d2cc932d1dd86c412e6e2555d78
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-04-12 18:09:35 +0300

    create cpu class

---
+++ freeboy/include/CPU.h
@@ -0,0 +1,14 @@
+//
+// Created by ozgur on 12/04/2024.
+//
+
+#ifndef FREEBOY_CPU_H
+#define FREEBOY_CPU_H
+
+
+class CPU {
+
+};
+
+
+#endif //FREEBOY_CPU_H
---
+++ freeboy/src/CPU.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ozgur on 12/04/2024.
+//
+
+#include "../include/CPU.h"
