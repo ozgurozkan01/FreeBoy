7043df003e44e11040d5f2a1f0e9058684956ed2
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-04-15 01:02:40 +0300

    create register template class

---
+++ freeboy/include/Register.h
@@ -0,0 +1,14 @@
+//
+// Created by ozgur on 4/15/2024.
+//
+
+#ifndef FREEBOY_REGISTER_H
+#define FREEBOY_REGISTER_H
+
+
+class Register {
+
+};
+
+
+#endif //FREEBOY_REGISTER_H
---
+++ freeboy/src/Register.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ozgur on 4/15/2024.
+//
+
+#include "../include/Register.h"
