1c02f7b7b468dbbac91cd8a8dc337d3344d72a8f
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-04-14 23:51:26 +0300

    add gameboy namespace

--- freeboy/include/Cartridge.h
+++ freeboy/include/Cartridge.h
@@ -9,122 +9,125 @@
 #include <cstdint>
 #include <string>
 
-enum class MBC_TYPE : uint8_t
+namespace gameboy
 {
-    ROM_ONLY = 0x00u,
-    MBC_1 = 0x01u,
-    MBC_1_RAM = 0x02u,
-    MBC_1_RAM_BATTERY = 0x03u,
-    MBC_2 = 0x05u,
-    MBC_2_BATTERY = 0x06u,
-    ROM_RAM = 0x08u,
-    ROM_RAM_BATTERY = 0x09u,
-    MMM_01 = 0x0Bu,
-    MMM_01_RAM = 0x0Cu,
-    MMM_01_RAM_BATTERY = 0x0Du,
-    MBC_3_TIMER_BATTERY = 0x0Fu,
-    MBC_3_TIMER_RAM_BATTERY = 0x10u,
-    MBC_3 = 0x11u,
-    MBC_3_RAM = 0x12u,
-    MBC_3_RAM_BATTERY = 0x13u,
-    MBC_5 = 0x19,
-    MBC_5_RAM = 0x1Au,
-    MBC_5_RAM_BATTERY = 0x1Bu,
-    MBC_5_RUMBLE = 0x1Cu,
-    MBC_5_RUMBLE_RAM = 0x1Du,
-    MBC_5_RUMBLE_RAM_BATTERY = 0x1Eu,
-    MBC_6 = 0x20u,
-    MBC_7_SENSOR_RUMBLE_RAM_BATTERY = 0x22u,
-    POCKET_CAMERA = 0xFCu,
-    BANDAI_TAMA_5 = 0xFDu,
-    HuC_3 = 0xFEu,
-    HuC_1_RAM_BATTERY = 0xFFu,
-};
+    enum class MBC_TYPE : uint8_t
+    {
+        ROM_ONLY = 0x00u,
+        MBC_1 = 0x01u,
+        MBC_1_RAM = 0x02u,
+        MBC_1_RAM_BATTERY = 0x03u,
+        MBC_2 = 0x05u,
+        MBC_2_BATTERY = 0x06u,
+        ROM_RAM = 0x08u,
+        ROM_RAM_BATTERY = 0x09u,
+        MMM_01 = 0x0Bu,
+        MMM_01_RAM = 0x0Cu,
+        MMM_01_RAM_BATTERY = 0x0Du,
+        MBC_3_TIMER_BATTERY = 0x0Fu,
+        MBC_3_TIMER_RAM_BATTERY = 0x10u,
+        MBC_3 = 0x11u,
+        MBC_3_RAM = 0x12u,
+        MBC_3_RAM_BATTERY = 0x13u,
+        MBC_5 = 0x19,
+        MBC_5_RAM = 0x1Au,
+        MBC_5_RAM_BATTERY = 0x1Bu,
+        MBC_5_RUMBLE = 0x1Cu,
+        MBC_5_RUMBLE_RAM = 0x1Du,
+        MBC_5_RUMBLE_RAM_BATTERY = 0x1Eu,
+        MBC_6 = 0x20u,
+        MBC_7_SENSOR_RUMBLE_RAM_BATTERY = 0x22u,
+        POCKET_CAMERA = 0xFCu,
+        BANDAI_TAMA_5 = 0xFDu,
+        HuC_3 = 0xFEu,
+        HuC_1_RAM_BATTERY = 0xFFu,
+    };
 
-enum class ROM_SIZE : uint8_t
-{
-    // kibibyte
-    KIB_32 = 0x00u,
-    KIB_64 = 0x01u,
-    KIB_128 = 0x02u,
-    KIB_256 = 0x03u,
-    KIB_512 = 0x04u,
-    // mebibyte
-    MIB_1 = 0x05u,
-    MIB_2 = 0x06u,
-    MIB_4 = 0x07u,
-    MIB_8 = 0x08u,
-    MIB_1_1 = 0x52u,
-    MIB_1_2 = 0x53u,
-    MIB_1_5 = 0x54u,
-};
+    enum class ROM_SIZE : uint8_t
+    {
+        // kibibyte
+        KIB_32 = 0x00u,
+        KIB_64 = 0x01u,
+        KIB_128 = 0x02u,
+        KIB_256 = 0x03u,
+        KIB_512 = 0x04u,
+        // mebibyte
+        MIB_1 = 0x05u,
+        MIB_2 = 0x06u,
+        MIB_4 = 0x07u,
+        MIB_8 = 0x08u,
+        MIB_1_1 = 0x52u,
+        MIB_1_2 = 0x53u,
+        MIB_1_5 = 0x54u,
+    };
 
-enum class RAM_SIZE : uint8_t
-{
-    NO_RAM = 0x00u,
-    UNUSED = 0x01u,
-    KIB_8 = 0x02u, // 1 bank
-    KIB_32 = 0x03u, // 4 banks of 8 KiB each
-    KIB_128 = 0x04u, // 16 banks of 8 KiB each
-    KIB_64 = 0x05u, // 8 banks of 8 KiB each
-};
+    enum class RAM_SIZE : uint8_t
+    {
+        NO_RAM = 0x00u,
+        UNUSED = 0x01u,
+        KIB_8 = 0x02u, // 1 bank
+        KIB_32 = 0x03u, // 4 banks of 8 KiB each
+        KIB_128 = 0x04u, // 16 banks of 8 KiB each
+        KIB_64 = 0x05u, // 8 banks of 8 KiB each
+    };
 
-enum class ROM_START_LOC : uint16_t
-{
-    ENTRY_POINT = 0x0100,
-    NINTENDO_LOGO = 0x0104,
-    TITLE = 0x0134,
-    MANUFACTURER_CODE = 0x013F,
-    CGB_FLAG = 0x0143,
-    NEW_LICENSEE_CODE = 0x0144,
-    SGB_FLAG = 0x146,
-    CARTRIDGE_TYPE = 0x0147,
-    ROM_SIZE = 0x0148,
-    RAM_SIZE = 0x0149,
-    DESTINATION_CODE = 0x014A,
-    OLD_LICENSEE_CODE = 0x014B,
-    MASK_ROM_VERSION_NUMBER = 0x014C,
-    HEADER_CHECKSUM = 0x014D,
-    GLOBAL_CHECKSUM = 0x014E,
-};
-
-
-struct RomHeader
-{
-    uint32_t entryPoint;
-    uint8_t nintendoLogo[0x30u];
-    char title[0x10u];
-    uint32_t manufacturerCode;
-    uint8_t cgbFlag;
-    uint16_t newLicenseeCode;
-    uint8_t sgbFlag;
-    MBC_TYPE mbcType;
-    ROM_SIZE romSize;
-    RAM_SIZE ramSize;
-    uint8_t destinationCode;
-    uint8_t oldLicenseeCode;
-    uint8_t maskRomVersionNumber;
-    uint8_t headerChecksum;
-    uint16_t globalChecksum;
-};
-
-class Cartridge {
-public:
-    Cartridge();
-    ~Cartridge();
-    bool loadCartridge(const std::string& cartridgeName);
-private:
-    char fileName[1024]{};
-    uint32_t romSize{};
-    uint8_t* romData{};
-    RomHeader romHeader{};
-
-    void loadRomHeader();
-    std::string getRamSize();
-    std::string getRomSize();
-    std::string getCartridgeTypeName();
-};
+    enum class ROM_START_LOC : uint16_t
+    {
+        ENTRY_POINT = 0x0100,
+        NINTENDO_LOGO = 0x0104,
+        TITLE = 0x0134,
+        MANUFACTURER_CODE = 0x013F,
+        CGB_FLAG = 0x0143,
+        NEW_LICENSEE_CODE = 0x0144,
+        SGB_FLAG = 0x146,
+        CARTRIDGE_TYPE = 0x0147,
+        ROM_SIZE = 0x0148,
+        RAM_SIZE = 0x0149,
+        DESTINATION_CODE = 0x014A,
+        OLD_LICENSEE_CODE = 0x014B,
+        MASK_ROM_VERSION_NUMBER = 0x014C,
+        HEADER_CHECKSUM = 0x014D,
+        GLOBAL_CHECKSUM = 0x014E,
+    };
 
 
+    struct RomHeader
+    {
+        uint32_t entryPoint;
+        uint8_t nintendoLogo[0x30u];
+        char title[0x10u];
+        uint32_t manufacturerCode;
+        uint8_t cgbFlag;
+        uint16_t newLicenseeCode;
+        uint8_t sgbFlag;
+        MBC_TYPE mbcType;
+        ROM_SIZE romSize;
+        RAM_SIZE ramSize;
+        uint8_t destinationCode;
+        uint8_t oldLicenseeCode;
+        uint8_t maskRomVersionNumber;
+        uint8_t headerChecksum;
+        uint16_t globalChecksum;
+    };
+
+    class Cartridge {
+    public:
+        Cartridge();
+        ~Cartridge();
+        bool loadCartridge(const std::string& cartridgeName);
+        void cartridgeWrite(const uint16_t& address, const uint8_t& value);
+        [[maybe_unused]] uint8_t cartridgeRead(const uint16_t& address);
+    private:
+        char fileName[1024]{};
+        uint32_t romSize{};
+        uint8_t* romData{};
+        RomHeader romHeader{};
+
+        void loadRomHeader();
+        std::string getRamSize();
+        std::string getRomSize();
+        std::string getCartridgeTypeName();
+    };
+}
 
 #endif //FREEBOY_CARTRIDGE_H
