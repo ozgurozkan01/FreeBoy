d971986d8e18eacbdad40b6b7c00970ba16a0781
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-04-15 03:51:43 +0300

    add CPU registers

--- freeboy/include/CPU.h
+++ freeboy/include/CPU.h
@@ -6,12 +6,14 @@
 #define FREEBOY_CPU_H
 
 #include <cstdint>
+#include "Register.h"
 
 namespace gameboy
 {
     enum class FLAG_TYPE : uint8_t
     {
         // LOWER 8 BITS OF AF
+        NONE = 0x0,
         ZERO_FLAG = 1 << 7,
         SUBTRACT_FLAG = 1 << 6, // (BCD)
         HALF_CARRY_FLAG = 1 << 5, // (BCD)
@@ -24,6 +26,15 @@ namespace gameboy
         bool init();
     private:
         uint16_t fetchData; // get the data for decoding
+
+        Register16 PC; // program counter
+        Register16 SP; // stack pointer
+        /* accumulator register */
+        Register16 AF; // A -> high, ZNHC0000 -> low
+        /* general purpose register */
+        Register16 BC; // B -> high, C -> low
+        Register16 DE; // D -> high, E -> low
+        Register16 HL; // H -> high, L -> low
     };
 }
 
