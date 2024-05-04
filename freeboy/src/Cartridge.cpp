b0a2a08bf9e5e557954acc8ee33da1544d4924d1
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-05-04 16:52:20 +0300

    refactor code

--- freeboy/src/Cartridge.cpp
+++ freeboy/src/Cartridge.cpp
@@ -4,12 +4,113 @@
 
 #include "../../freeboy/include/Cartridge.h"
 #include <fstream>
+#include <unordered_map>
+
+constexpr uint16_t HEADER_START_POINT = 0x0100;
 
 namespace gameboy
 {
-    Cartridge::Cartridge()
-    {
-    }
+    static const std::string ROM_TYPES[] = {
+            "ROM ONLY",                               // 0x00
+            "MBC1",                                   // 0x01
+            "MBC1+RAM",                               // 0x02
+            "MBC1+RAM+BATTERY",                       // 0x03
+            "0x04 ???",                               // 0x04
+            "MBC2",                                   // 0x05
+            "MBC2+BATTERY",                           // 0x06
+            "0x07 ???",                               // 0x07
+            "ROM+RAM 1",                              // 0x08
+            "ROM+RAM+BATTERY 1",                      // 0x09
+            "0x0A ???",                               // 0x0A
+            "MMM01",                                  // 0x0B
+            "MMM01+RAM",                              // 0x0C
+            "MMM01+RAM+BATTERY",                      // 0x0D
+            "0x0E ???",                               // 0x0E
+            "MBC3+timer+BATTERY",                     // 0x0F
+            "MBC3+timer+RAM+BATTERY 2",               // 0x10
+            "MBC3",                                   // 0x11
+            "MBC3+RAM 2",                             // 0x12
+            "MBC3+RAM+BATTERY 2",                     // 0x13
+            "0x14 ???",                               // 0x14
+            "0x15 ???",                               // 0x15
+            "0x16 ???",                               // 0x16
+            "0x17 ???",                               // 0x17
+            "0x18 ???",                               // 0x18
+            "MBC5",                                   // 0x19
+            "MBC5+RAM",                               // 0x1A
+            "MBC5+RAM+BATTERY",                       // 0x1B
+            "MBC5+RUMBLE",                            // 0x1C
+            "MBC5+RUMBLE+RAM",                        // 0x1D
+            "MBC5+RUMBLE+RAM+BATTERY",                // 0x1E
+            "0x1F ???",                               // 0x1F
+            "MBC6",                                   // 0x20
+            "0x21 ???",                               // 0x21
+            "MBC7+SENSOR+RUMBLE+RAM+BATTERY"          // 0x22
+    };
+
+    static const std::unordered_map<uint8_t , std::string> LICENCE_CODE = {
+            {0x00, "None"},
+            {0x01, "Nintendo R&D1"},
+            {0x08, "Capcom"},
+            {0x13, "Electronic Arts"},
+            {0x18, "Hudson Soft"},
+            {0x19, "b-ai"},
+            {0x20, "kss"},
+            {0x22, "pow"},
+            {0x24, "PCM Complete"},
+            {0x25, "san-x"},
+            {0x28, "Kemco Japan"},
+            {0x29, "seta"},
+            {0x30, "Viacom"},
+            {0x31, "Nintendo"},
+            {0x32, "Bandai"},
+            {0x33, "Ocean/Acclaim"},
+            {0x34, "Konami"},
+            {0x35, "Hector"},
+            {0x37, "Taito"},
+            {0x38, "Hudson"},
+            {0x39, "Banpresto"},
+            {0x41, "Ubi Soft"},
+            {0x42, "Atlus"},
+            {0x44, "Malibu"},
+            {0x46, "angel"},
+            {0x47, "Bullet-Proof"},
+            {0x49, "irem"},
+            {0x50, "Absolute"},
+            {0x51, "Acclaim"},
+            {0x52, "Activision"},
+            {0x53, "American sammy"},
+            {0x54, "Konami"},
+            {0x55, "Hi tech entertainment"},
+            {0x56, "LJN"},
+            {0x57, "Matchbox"},
+            {0x58, "Mattel"},
+            {0x59, "Milton Bradley"},
+            {0x60, "Titus"},
+            {0x61, "Virgin"},
+            {0x64, "LucasArts"},
+            {0x67, "Ocean"},
+            {0x69, "Electronic Arts"},
+            {0x70, "Infogrames"},
+            {0x71, "Interplay"},
+            {0x72, "Broderbund"},
+            {0x73, "sculptured"},
+            {0x75, "sci"},
+            {0x78, "THQ"},
+            {0x79, "Accolade"},
+            {0x80, "misawa"},
+            {0x83, "lozc"},
+            {0x86, "Tokuma Shoten Intermedia"},
+            {0x87, "Tsukuda Original"},
+            {0x91, "Chunsoft"},
+            {0x92, "Video system"},
+            {0x93, "Ocean/Acclaim"},
+            {0x95, "Varie"},
+            {0x96, "Yonezawa/s’pal"},
+            {0x97, "Kaneko"},
+            {0x99, "Pack in soft"},
+            {0xA4, "Konami (Yu-Gi-Oh!)"}
+    };
 
     bool Cartridge::loadCartridge(const std::string& cartridgeName)
     {
@@ -23,183 +124,71 @@ namespace gameboy
 
         cartridgeFile.seekg(0, std::ios::end);
 
-        romSize = cartridgeFile.tellg();
+        romSize = static_cast<uint32_t>(cartridgeFile.tellg());
         romData = new uint8_t[romSize];
 
         cartridgeFile.seekg(std::ios::beg);
 
-        if (!cartridgeFile.read((char*) romData, romSize))
+        if (!cartridgeFile.read(reinterpret_cast<char*>(romData), romSize))
         {
             printf("ROM file could not be read properly.\n");
+            cartridgeFile.close();
             return false;
         }
         cartridgeFile.close();
 
-        loadRomHeader();
+        // LOAD ROM HEADER
+        romHeader = reinterpret_cast<RomHeader*>(romData + HEADER_START_POINT);
+        // PRINT HEADER INFORMATIONS
+        printf("Successfully loaded ROM from file path: %s:\n", cartridgeName.c_str());
+        printf("\tTitle    : %s\n", romHeader->title);
+        printf("\tType     : %2.2X (%s)\n", romHeader->cartridgeType, getCartridgeType().c_str());
+        printf("\tROM Size : %d KB\n", 32 << romHeader->romSize);
+        printf("\tRAM Size : %2.2X\n", romHeader->ramSize);
+        printf("\tLIC Code : %2.2X (%s)\n", romHeader->oldLicenseeCode, getCartridgeLicence().c_str());
+        printf("\tROM Vers : %2.2X\n", romHeader->romVersion);
+        printf("\tChecksum : %2.2X (%s)\n", romHeader->headerChecksum, isChecksumPassed() ? "PASSED" : "FAILED");
 
         return true;
     }
 
-    std::string Cartridge::getCartridgeTypeName()
+    std::string Cartridge::getCartridgeType()
     {
-        switch (romHeader.mbcType)
+        if (romHeader->cartridgeType <= 0x22)
         {
-            case MBC_TYPE::ROM_ONLY:
-                return "ROM_ONLY";
-            case MBC_TYPE::MBC_1:
-                return "MBC_1";
-            case MBC_TYPE::MBC_1_RAM:
-                return "MBC_1_RAM";
-            case MBC_TYPE::MBC_1_RAM_BATTERY:
-                return "MBC_1_RAM_BATTERY";
-            case MBC_TYPE::MBC_2:
-                return "MBC_2";
-            case MBC_TYPE::MBC_2_BATTERY:
-                return "MBC_2_BATTERY";
-            case MBC_TYPE::ROM_RAM:
-                return "ROM_RAM";
-            case MBC_TYPE::ROM_RAM_BATTERY:
-                return "ROM_RAM_BATTERY";
-            case MBC_TYPE::MMM_01:
-                return "MMM_01";
-            case MBC_TYPE::MMM_01_RAM:
-                return "MMM_01_RAM";
-            case MBC_TYPE::MMM_01_RAM_BATTERY:
-                return "MMM_01_RAM_BATTERY";
-            case MBC_TYPE::MBC_3_TIMER_BATTERY:
-                return "MBC_3_TIMER_BATTERY";
-            case MBC_TYPE::MBC_3_TIMER_RAM_BATTERY:
-                return "MBC_3_TIMER_RAM_BATTERY";
-            case MBC_TYPE::MBC_3:
-                return "MBC_3";
-            case MBC_TYPE::MBC_3_RAM:
-                return "MBC_3_RAM";
-            case MBC_TYPE::MBC_3_RAM_BATTERY:
-                return "MBC_3_RAM_BATTERY";
-            case MBC_TYPE::MBC_5:
-                return "MBC_5";
-            case MBC_TYPE::MBC_5_RAM:
-                return "MBC_5_RAM";
-            case MBC_TYPE::MBC_5_RAM_BATTERY:
-                return "MBC_5_RAM_BATTERY";
-            case MBC_TYPE::MBC_5_RUMBLE:
-                return "MBC_5_RUMBLE";
-            case MBC_TYPE::MBC_5_RUMBLE_RAM:
-                return "MBC_5_RUMBLE_RAM";
-            case MBC_TYPE::MBC_5_RUMBLE_RAM_BATTERY:
-                return "MBC_5_RUMBLE_RAM_BATTERY";
-            case MBC_TYPE::MBC_6:
-                return "MBC_6";
-            case MBC_TYPE::MBC_7_SENSOR_RUMBLE_RAM_BATTERY:
-                return "MBC_7_SENSOR_RUMBLE_RAM_BATTERY";
-            case MBC_TYPE::POCKET_CAMERA:
-                return "POCKET_CAMERA";
-            case MBC_TYPE::BANDAI_TAMA_5:
-                return "BANDAI_TAMA_5";
-            case MBC_TYPE::HuC_3:
-                return "HuC_3";
-            case MBC_TYPE::HuC_1_RAM_BATTERY:
-                return "HuC_1_RAM_BATTERY";
+            return ROM_TYPES[romHeader->cartridgeType];
         }
-        return "";
+
+        return "UNKNOWN";
+        exit(-1);
     }
 
-    Cartridge::~Cartridge()
+    std::string Cartridge::getCartridgeLicence()
     {
-    }
-
-    std::string Cartridge::getRamSize() {
-        switch (romHeader.ramSize) {
-            case RAM_SIZE::NO_RAM:
-                return "NO_RAM";
-            case RAM_SIZE::UNUSED:
-                return "UNUSED";
-            case RAM_SIZE::KIB_8:
-                return "KIB_8";
-            case RAM_SIZE::KIB_32:
-                return "KIB_32";
-            case RAM_SIZE::KIB_128:
-                return "KIB_128";
-            case RAM_SIZE::KIB_64:
-                return "KIB_64";
+        if (romHeader->newLicenseeCode <= 0xA4)
+        {
+            auto licence = LICENCE_CODE.find( romHeader->oldLicenseeCode);
+            if (licence != LICENCE_CODE.end())
+            {
+                return licence->second;
+            }
         }
-        return "";
+        return "UNKNOWN";
+        exit(-1);
     }
 
-    std::string Cartridge::getRomSize()
+    bool Cartridge::isChecksumPassed()
     {
-        switch (romHeader.romSize)
+        uint8_t checksum = 0;
+        for (uint16_t address = 0x0134; address <= 0x014C; address++)
         {
-            case ROM_SIZE::KIB_32:
-                return "KIB_32";
-            case ROM_SIZE::KIB_64:
-                return "KIB_64";
-            case ROM_SIZE::KIB_128:
-                return "KIB_128";
-            case ROM_SIZE::KIB_256:
-                return "KIB_256";
-            case ROM_SIZE::KIB_512:
-                return "KIB_512";
-            case ROM_SIZE::MIB_1:
-                return "MIB_1";
-            case ROM_SIZE::MIB_2:
-                return "MIB_2";
-            case ROM_SIZE::MIB_4:
-                return "MIB_4";
-            case ROM_SIZE::MIB_8:
-                return "MIB_8";
-            case ROM_SIZE::MIB_1_1:
-                return "MIB_1_1";
-            case ROM_SIZE::MIB_1_2:
-                return "MIB_1_2";
-            case ROM_SIZE::MIB_1_5:
-                return "MIB_1_5";
+            checksum = checksum - romData[address] - 1;
         }
-        return "";
+        /* The boot ROM verifies this checksum. If the byte at $014D(Header Checksum) does not match the lower 8 bits of checksum,
+         * the boot ROM will lock up and the program in the cartridge won’t run.*/
+        return  romHeader->headerChecksum == (checksum & 0xFF);
     }
 
-    void Cartridge::loadRomHeader()
-    {
-        // ROM HEADER
-        romHeader.entryPoint = romData[static_cast<uint16_t>(ROM_START_LOC::ENTRY_POINT)];
-
-        for (uint16_t i = static_cast<uint16_t>(ROM_START_LOC::NINTENDO_LOGO), index = 0; i < static_cast<uint16_t>(ROM_START_LOC::TITLE); ++i, ++index)
-        {
-            romHeader.nintendoLogo[index] = romData[i];
-        }
-        for (uint16_t i = static_cast<uint16_t>(ROM_START_LOC::TITLE), index = 0; i < static_cast<uint16_t>(ROM_START_LOC::MANUFACTURER_CODE); ++i, ++index)
-        {
-            romHeader.title[index] = (char)romData[i];
-        }
-        romHeader.manufacturerCode = romData[static_cast<uint16_t>(ROM_START_LOC::MANUFACTURER_CODE)];
-        romHeader.cgbFlag = romData[static_cast<uint16_t>(ROM_START_LOC::CGB_FLAG)];
-        romHeader.newLicenseeCode = romData[static_cast<uint16_t>(ROM_START_LOC::NEW_LICENSEE_CODE)];
-        romHeader.sgbFlag = romData[static_cast<uint16_t>(ROM_START_LOC::SGB_FLAG)];
-        romHeader.mbcType = static_cast<MBC_TYPE>(romData[static_cast<uint16_t>(ROM_START_LOC::CARTRIDGE_TYPE)]);
-        romHeader.romSize = static_cast<ROM_SIZE>(romData[static_cast<uint16_t>(ROM_START_LOC::ROM_SIZE)]);
-        romHeader.ramSize = static_cast<RAM_SIZE>(romData[static_cast<uint16_t>(ROM_START_LOC::RAM_SIZE)]);
-        romHeader.destinationCode = romData[static_cast<uint16_t>(ROM_START_LOC::DESTINATION_CODE)];
-        romHeader.oldLicenseeCode = romData[static_cast<uint16_t>(ROM_START_LOC::OLD_LICENSEE_CODE)];
-        romHeader.maskRomVersionNumber = romData[static_cast<uint16_t>(ROM_START_LOC::MASK_ROM_VERSION_NUMBER)];
-        romHeader.headerChecksum = romData[static_cast<uint16_t>(ROM_START_LOC::HEADER_CHECKSUM)];
-        romHeader.globalChecksum = romData[static_cast<uint16_t>(ROM_START_LOC::GLOBAL_CHECKSUM)];
-
-        printf("Cartridge Loaded:\n");
-        printf("\t Title    : %s\n", romHeader.title);
-        printf("\t Type     : %s\n", getCartridgeTypeName().c_str());
-        printf("\t ROM Size : %s\n", getRomSize().c_str());
-        printf("\t RAM Size : %s\n", getRamSize().c_str());
-        printf("\t ROM Vers : %2.2X\n", romHeader.maskRomVersionNumber);
-
-        uint16_t x = 0;
-        for (uint16_t i=0x0134; i<=0x014C; i++) {
-            x = x - romData[i] - 1;
-        }
-        printf("\t Checksum : %2.2X (%s)\n", romHeader.headerChecksum, (x & 0xFF) ? "PASSED" : "FAILED");
-
-    }
-
-    void Cartridge::cartridgeWrite(const registers::Register16& address, const uint8_t &value) {}
-    uint8_t Cartridge::cartridgeRead(const registers::Register16& address) { return 0; }
-
+    void Cartridge::writeCartridge(uint16_t address, uint8_t value) { }
+    uint8_t Cartridge:: readCartridge(uint16_t address) { return romData[address]; }
 }
