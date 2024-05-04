81fe152beb1e828fd498e53d363a164cc6b3556a
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-05-04 16:25:58 +0300

    simplify class by taking off redundant implementations

--- freeboy/include/Bus.h
+++ freeboy/include/Bus.h
@@ -5,10 +5,23 @@
 #ifndef FREEBOY_BUS_H
 #define FREEBOY_BUS_H
 
+#include <cstdint>
 
-class Bus {
+namespace gameboy
+{
+    class Cartridge;
 
-};
+    class Bus {
+    public:
+        Bus(Cartridge* _cartridge);
+        ~Bus();
 
+        [[nodiscard]] static bool isMemoryAddressExist(uint16_t address);
+        void busWrite(uint16_t  address, uint8_t value);
+        uint8_t busRead(uint16_t  address);
+    private:
+        Cartridge* cartridgeRef;
+    };
+}
 
 #endif //FREEBOY_BUS_H
