0772d48f14d793a94b13579458832a115d104b3c
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-05-07 01:05:46 +0300

    update CPU instruction methods parameters

--- freeboy/src/CPU_Instructions.cpp
+++ freeboy/src/CPU_Instructions.cpp
@@ -6,9 +6,9 @@
 
 namespace gameboy::instruction
 {
-    void CPU_Instructions::NOP() {}
-    void CPU_Instructions::LD() {}
-    void CPU_Instructions::JP() {}
-    void CPU_Instructions::XOR() {}
-    void CPU_Instructions::DEC() {}
+    void CPU_Instructions::NOP(CPU* _cpu) {}
+    void CPU_Instructions::LD(CPU* _cpu) {}
+    void CPU_Instructions::JP(CPU* _cpu) {}
+    void CPU_Instructions::XOR(CPU* _cpu) {}
+    void CPU_Instructions::DEC(CPU* _cpu) {}
 }
