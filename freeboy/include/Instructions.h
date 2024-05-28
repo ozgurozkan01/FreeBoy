77fc2fbe8a9bcbef492dacadc0c7339e95efb94e
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-05-28 23:53:35 +0300

    refactor code base

--- freeboy/include/Instructions.h
+++ freeboy/include/Instructions.h
@@ -7,9 +7,6 @@
 
 #include <cstdint>
 #include <array>
-#include <string_view>
-#include <unordered_map>
-#include <vector>
 
 namespace gameboy {
 
@@ -25,23 +22,8 @@ namespace gameboy {
             Z       // Execute if Z is set.
         };
 
-        enum class RegisterType : uint8_t {
-             NONE,
-             A,
-             F,
-             B,
-             C,
-             D,
-             E,
-             H,
-             L,
-             AF,
-             BC,
-             DE,
-             HL,
-             SP,
-             PC
-        };
+        enum class RegisterType : uint8_t { NONE, A, F, B, C, D, E, H, L,
+                                            AF, BC, DE, HL, SP, PC };
 
         class Instructions
         {
@@ -50,8 +32,8 @@ namespace gameboy {
             {
                 uint8_t length = 0;
                 uint8_t tCycles = 0;
-                void (gameboy::CPU::*operation)();
-                void (gameboy::CPU::*addrMode)();
+                void (gameboy::CPU::*operation)() = nullptr;
+                void (gameboy::CPU::*addrMode)() = nullptr;
                 RegisterType dstRegister = RegisterType::NONE;
                 RegisterType srcRegister = RegisterType::NONE;
                 ConditionCode condition = ConditionCode::NONE;
@@ -64,7 +46,6 @@ namespace gameboy {
 
             std::array<Info, 0x100> standardInstructions;
             std::array<Info, 0x100> cbInstructions;
-            std::vector<std::string_view> lookUp;
         private:
             CPU* cpuRef;
         };
