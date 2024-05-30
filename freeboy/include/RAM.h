6ef5a540204de4c76b9dcfa4e3fdef5292555c4d
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-05-31 00:39:54 +0300

    init RAM class

---
+++ freeboy/include/RAM.h
@@ -0,0 +1,14 @@
+//
+// Created by ozgur on 5/31/2024.
+//
+
+#ifndef FREEBOY_RAM_H
+#define FREEBOY_RAM_H
+
+
+class RAM {
+
+};
+
+
+#endif //FREEBOY_RAM_H
---
+++ freeboy/src/RAM.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ozgur on 5/31/2024.
+//
+
+#include "../include/RAM.h"
