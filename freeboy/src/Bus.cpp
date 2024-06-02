be5703ea82c670e7a90714d2781864bc3cffc309
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-06-02 11:06:38 +0300

    complete addition of memory map ranges

--- freeboy/src/Bus.cpp
+++ freeboy/src/Bus.cpp
@@ -4,21 +4,91 @@
 
 #include "../include/Bus.h"
 #include "../include/Cartridge.h"
+#include "../include/InterruptHandler.h"
+#include "../include/RAM.h"
+
+/*
+ * 0x0000 - 0x3FFF : ROM Bank 0 (ROM DATA)
+ * 0x4000 - 0x7FFF : ROM Bank 1 - Switchable (ROM DATA)
+ * ------------------------------------------
+ * 0x8000 - 0x97FF : CHR RAM (VRAM)
+ * 0x9800 - 0x9BFF : BG Map 1 (VRAM)
+ * 0x9C00 - 0x9FFF : BG Map 2 (VRAM)
+ * ------------------------------------------
+ * 0xA000 - 0xBFFF : Cartridge RAM (Cartridge External)
+ * ------------------------------------------
+ * 0xC000 - 0xCFFF : RAM Bank 0 (WRAM)
+ * 0xD000 - 0xDFFF : RAM Bank 1-7 - switchable - Color only (WRAM)
+ * ------------------------------------------
+ * 0xE000 - 0xFDFF : Reserved - Echo RAM
+ * ------------------------------------------
+ * 0xFE00 - 0xFE9F : Object Attribute Memory
+ * ------------------------------------------
+ * 0xFEA0 - 0xFEFF : Reserved - Unusable
+ * ------------------------------------------
+ * 0xFF00 - 0xFF7F : I/O Registers
+ * ------------------------------------------
+ * 0xFF80 - 0xFFFE : Zero Page (HRAM)
+ * ------------------------------------------
+ * 0xFFFF - 0xFFFF : Enable Interrupt Register
+ */
 
 namespace gameboy
 {
-    Bus::Bus(Cartridge* _cartridge) : cartridgeRef(_cartridge) {}
+    Bus::Bus(Cartridge* _cartridge, instruction::InterruptHandler* _interruptHandler) :
+    cartridgeRef(_cartridge),
+    interruptHandlerRef(_interruptHandler),
+    wram(new ram::WRAM)
+    {}
+
     Bus::~Bus() {}
 
     void Bus::write8(uint16_t address, uint8_t value)
     {
-        if (address < 0x8000)
+        if (address < 0x8000) // ROM DATA
         {
             cartridgeRef->write(address, value);
             return;
         }
+        else if (address < 0xA000) // VRAM (Video RAM)
+        {
+            return;
+        }
+        else if (address < 0xC000) // Cartridge External RAM
+        {
+            return;
+        }
+        else if (address < 0xE000) // WRAM
+        {
+            return;
+        }
+        else if (address < 0xFE00) // ECHO RAM
+        {
+            return;
+        }
+        else if(address < 0xFEA0) // Object Attribute Memory (OAM)
+        {
+            return;
+        }
+        else if(address < 0xFF00) // NOT USABLE
+        {
+            return;
+        }
+        else if(address < 0xFF80) // I/O Registers
+        {
+            return;
+        }
+        else if(address < 0xFFFF) // HRAM (High RAM)
+        {
+            return;
+        }
+        else if(address == 0xFFFF) // Enable Interrupt Register
+        {
+            interruptHandlerRef->setIE(value);
+            return;
+        }
 
-        printf("Address does not avaible to write !\n");
+        printf("UNSUPPORTED bus_write(%04X)\n", address);
     }
 
     void Bus::write16(uint16_t address, uint16_t value)
@@ -33,8 +103,44 @@ namespace gameboy
         {
             return cartridgeRef->read(address);
         }
+        else if (address < 0xA000) // VRAM (Video RAM)
+        {
+            return -1;
+        }
+        else if (address < 0xC000) // Cartridge External RAM
+        {
+            return cartridgeRef->read(address);
+        }
+        else if (address < 0xE000) // WRAM (Working RAM)
+        {
+            return wram->read(address);
+        }
+        else if (address < 0xFE00) // ECHO RAM
+        {
+            return -1;
+        }
+        else if(address < 0xFEA0) // Object Attribute Memory (OAM)
+        {
+            return -1;
+        }
+        else if(address < 0xFF00) // NOT USABLE
+        {
+            return -1;
+        }
+        else if(address < 0xFF80) // I/O Registers
+        {
+            return -1;
+        }
+        else if(address < 0xFFFF) // HRAM (High RAM)
+        {
+            return -1;
+        }
+        else if(address == 0xFFFF) // Enable Interrupt Register
+        {
+            return interruptHandlerRef->getIE();
+        }
 
-        printf("Address does not avaible to read !\n");
+        printf("UNSUPPORTED bus_read(%04X)\n", address);
         return -1;
     }
 
