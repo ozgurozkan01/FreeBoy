6b851c8dc9e86faeb70511c20200019f7830e9d3
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-06-08 14:39:59 +0300

    init class

---
+++ freeboy/include/CPURegisters.h
@@ -0,0 +1,14 @@
+//
+// Created by ozgur on 6/8/2024.
+//
+
+#ifndef FREEBOY_CPUREGISTERS_H
+#define FREEBOY_CPUREGISTERS_H
+
+
+class CPURegisters {
+
+};
+
+
+#endif //FREEBOY_CPUREGISTERS_H
---
+++ freeboy/src/CPURegisters.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ozgur on 6/8/2024.
+//
+
+#include "../include/CPURegisters.h"
