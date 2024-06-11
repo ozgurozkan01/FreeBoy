07fb292fe6de88d25d7ad603ffe95b618a627456
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-06-12 00:28:38 +0300

    complete refactor implementation

--- freeboy/include/InstructionSet.h
+++ freeboy/include/InstructionSet.h
@@ -7,41 +7,27 @@
 
 #include <cstdint>
 #include <array>
+#include <string>
 
-namespace gameboy {
-
-    class CPU;
-
-    namespace instruction {
-
-        enum class ConditionCode : uint8_t {
-            NONE,   // Nothing
-            NC,     // Execute if C is not set.
-            C,      // Execute if C is set.
-            NZ,     // Execute if Z is not set.
-            Z       // Execute if Z is set.
-        };
-
-        enum class RegisterType : uint8_t { NONE, A, F, B, C, D, E, H, L,
-                                            AF, BC, DE, HL, SP, PC };
-
-        class Instructions
+namespace gameboy
+{
+    namespace instruction
+    {
+        class InstructionSet
         {
         public:
-            struct Info
+
+            struct Header
             {
-                uint8_t length = 0;
-                uint8_t mCycles = 0;
+                std::string nmenomic;
+                uint8_t length;
+                uint8_t mCycles;
             };
 
-            Instructions(CPU* _cpu);
+            InstructionSet();
 
-            bool init();
-
-            std::array<Info, 0x100> standardInstructions;
-            std::array<Info, 0x100> cbInstructions;
-        private:
-            CPU* cpuRef;
+            std::array<Header, 0x100> standardInstructions;
+            std::array<Header, 0x100> cbInstructions;
         };
     }
 }
