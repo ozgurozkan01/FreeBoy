88b2dccea1bd1f6417ca6d94c390ed87c7bfbef7
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-06-12 17:18:31 +0300

    init alu class

---
+++ freeboy/include/ALU.h
@@ -0,0 +1,14 @@
+//
+// Created by ozgur on 6/12/2024.
+//
+
+#ifndef FREEBOY_ALU_H
+#define FREEBOY_ALU_H
+
+
+class ALU {
+
+};
+
+
+#endif //FREEBOY_ALU_H
---
+++ freeboy/src/ALU.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ozgur on 6/12/2024.
+//
+
+#include "../include/ALU.h"
