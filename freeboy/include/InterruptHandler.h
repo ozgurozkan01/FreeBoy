6cdec2088039a626f7b6519e89f51239b2155c23
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-06-15 16:07:34 +0300

    add IE and IF register

--- freeboy/include/InterruptHandler.h
+++ freeboy/include/InterruptHandler.h
@@ -5,15 +5,36 @@
 #ifndef FREEBOY_INTERRUPTHANDLER_H
 #define FREEBOY_INTERRUPTHANDLER_H
 
+#include <cstdint>
+
 namespace gameboy
 {
+    enum class InterruptFlags : uint8_t
+    {
+        vblank = 0,
+        lcd = 1,
+        timer = 2,
+        serial = 3,
+        joypad = 4
+    };
+
     class InterruptHandler {
     public:
+
         InterruptHandler() : IME(false) {}
 
-        void setIME(bool _isEnable) { IME = _isEnable; };
+        void setIME(const bool _isEnable) { IME = _isEnable; };
+        void setIE(const uint8_t _value) { IE = _value; };
+        void setIF(const uint8_t _value) { IF = _value; };
+
+        bool getIME() const { return IME; }
+        uint8_t getIE() const { return IE; }
+        uint8_t getIF() const { return IF; }
+
     private:
         bool IME;
+        uint8_t IE;
+        uint8_t IF;
     };
 }
 
