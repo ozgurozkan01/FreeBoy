8ce802b4c98f8455ea9950dddba9d289fbe57285
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-06-19 19:07:00 +0300

    add arithmetic and logic operations

--- freeboy/include/ALU.h
+++ freeboy/include/ALU.h
@@ -20,17 +20,28 @@ namespace gameboy
 
         bool init();
 
-        void xor_(const Register8& _register);
+        void xor_(const Register8& _srcRegister);
         void xor_(const uint8_t _value);
 
         void or_(const Register8& _srcRegister);
         void or_(const uint8_t _value);
 
-        void increment(Register16& _register);
-        void increment(Register8& _register);
+        void and_(const Register8 _srcRegister);
+        void and_(const uint8_t _value);
 
-        void decrement(Register16& _register);
-        void decrement(Register8& _register);
+        void increment(Register16& _srcRegister);
+        void increment(Register8& _srcRegister);
+
+        void decrement(Register16& _srcRegister);
+        void decrement(Register8& _srcRegister);
+
+        void compare(Register8& _srcRegister);
+        void compare(const uint8_t _value);
+
+        void add(const Register8& _srcRegister);
+        void add(const Register16& _srcRegister);
+        void addToStack(const int8_t _value);
+        void add(const uint8_t _value);
 
     private:
         CPU* cpuPtr;
--- freeboy/src/ALU.cpp
+++ freeboy/src/ALU.cpp
@@ -40,9 +40,9 @@ namespace gameboy
         cpuPtr->resetFlag(CPUFlags::c);
     }
 
-    void ALU::xor_(const Register8& _register)
+    void ALU::xor_(const Register8& _srcRegister)
     {
-        xor_(_register.read());
+        xor_(_srcRegister.read());
     }
 
     void ALU::xor_(const uint8_t _value)
@@ -57,35 +57,93 @@ namespace gameboy
         cpuPtr->resetFlag(CPUFlags::c);
     }
 
-    void ALU::increment(Register16 &_register)
+    void ALU::increment(Register16 &_srcRegister)
     {
-        _register++;
+        _srcRegister++;
     }
 
-    void ALU::increment(Register8 &_register)
+    void ALU::increment(Register8 &_srcRegister)
     {
-        _register++;
+        _srcRegister++;
 
-        uint8_t result = _register.read();
+        uint8_t result = _srcRegister.read();
 
         result == 0x00 ? cpuPtr->setFlag(CPUFlags::z) : cpuPtr->resetFlag(CPUFlags::z);
         cpuPtr->resetFlag(CPUFlags::n);
         (result & 0xF) == 0 ? cpuPtr->setFlag(CPUFlags::h) : cpuPtr->resetFlag(CPUFlags::h);
     }
 
-    void ALU::decrement(Register16 &_register)
+    void ALU::decrement(Register16 &_srcRegister)
     {
-        _register--;
+        _srcRegister--;
     }
 
-    void ALU::decrement(Register8 &_register)
+    void ALU::decrement(Register8 &_srcRegister)
     {
-        _register--;
+        _srcRegister--;
 
-        uint8_t result = _register.read();
+        uint8_t result = _srcRegister.read();
 
         result == 0x00 ? cpuPtr->setFlag(CPUFlags::z) : cpuPtr->resetFlag(CPUFlags::z);
         cpuPtr->setFlag(CPUFlags::n);
         (result & 0xF) == 0xF ? cpuPtr->setFlag(CPUFlags::h) : cpuPtr->resetFlag(CPUFlags::h);
+    }
+
+    void ALU::compare(Register8 &_srcRegister)
+    {
+
+    }
+
+    void ALU::compare(const uint8_t _value)
+    {
+        uint8_t result = cpuPtr->AF.highByte().read() - _value;
+
+        result == 0x00 ? cpuPtr->setFlag(CPUFlags::z) : cpuPtr->resetFlag(CPUFlags::z);
+        cpuPtr->setFlag(CPUFlags::n);
+        (result & 0xF) == 0xF ? cpuPtr->setFlag(CPUFlags::h) : cpuPtr->resetFlag(CPUFlags::h);
+        _value > cpuPtr->AF.highByte().read() ? cpuPtr->setFlag(CPUFlags::c) : cpuPtr->resetFlag(CPUFlags::c);
+    }
+
+    void ALU::add(const uint8_t _value)
+    {
+        cpuPtr->AF.highByte() += _value;
+        uint16_t result = cpuPtr->AF.highByte().read();
+
+        result == 0x00 ? cpuPtr->setFlag(CPUFlags::z) : cpuPtr->resetFlag(CPUFlags::z);
+        cpuPtr->resetFlag(CPUFlags::n);
+        (result & 0xF) == 0 ? cpuPtr->setFlag(CPUFlags::h) : cpuPtr->resetFlag(CPUFlags::h);
+        (result > 0xFF) ? cpuPtr->setFlag(CPUFlags::c) : cpuPtr->resetFlag(CPUFlags::c);
+    }
+
+    void ALU::add(const Register8 &_srcRegister)
+    {
+        add(_srcRegister.read());
+    }
+
+    void ALU::add(const Register16 &_srcRegister)
+    {
+        cpuPtr->HL += _srcRegister;
+    }
+
+    void ALU::addToStack(const int8_t _value)
+    {
+
+    }
+
+    void ALU::and_(const Register8 _srcRegister)
+    {
+        and_(_srcRegister.read());
+    }
+
+    void ALU::and_(const uint8_t _value)
+    {
+        cpuPtr->AF.highByte() &= _value;
+
+        uint8_t result = cpuPtr->AF.highByte().read();
+
+        result == 0x00 ? cpuPtr->setFlag(CPUFlags::z) : cpuPtr->resetFlag(CPUFlags::z);
+        cpuPtr->resetFlag(CPUFlags::n);
+        cpuPtr->setFlag(CPUFlags::h);
+        cpuPtr->resetFlag(CPUFlags::c);
     }
 }
