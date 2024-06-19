02cdc2dd2c9f7715a01c529c69a535882ec2fa06
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-06-19 19:03:51 +0300

    add register16 and register8 classes

--- freeboy/include/Registers.h
+++ freeboy/include/Registers.h
@@ -5,10 +5,67 @@
 #ifndef FREEBOY_REGISTERS_H
 #define FREEBOY_REGISTERS_H
 
+#include <cstdint>
 
-class Registers {
+namespace gameboy
+{
+    namespace cpu_register
+    {
+        class Register8{
+        public:
+            Register8() = default;
+            Register8(uint8_t _value);
 
-};
+            constexpr uint8_t read() const { return value; };
+            constexpr void write(const uint8_t _value) { value = _value; };
 
+            uint8_t operator++(int); // Post-Increment
+            uint8_t operator++(); // Pre-Increment
+
+            uint8_t operator--(int); // Post-Increment
+            uint8_t operator--(); // Pre-Increment
+
+            void operator+=(const uint8_t _value);
+            void operator-=(const uint8_t _value);
+            void operator=(const Register8& _register);
+
+            void operator^=(const Register8& _register);
+            void operator|=(const Register8& _register);
+            void operator&=(const uint16_t _value);
+        private:
+            uint8_t value;
+        };
+
+        /* -------------------------------------------------------------------------- */
+
+        class Register16{
+        public:
+            Register16() = default;
+            Register16(uint16_t _value);
+
+            Register8& highByte() { return high; };
+            Register8& lowByte() { return low; };
+
+            uint16_t read() const { return (high.read() << 8) | low.read(); };
+            void write(uint16_t _value);
+
+            uint16_t operator++(int); // Post-Increment
+            uint16_t operator++(); // Pre-Increment
+
+            uint16_t operator--(int); // Post-Increment
+            uint16_t operator--(); // Pre-Increment
+
+            void operator+=(const Register16 _register);
+
+            void operator+=(const uint8_t _value);
+            void operator-=(const uint8_t _value);
+            void operator=(const Register16& _register);
+            void operator=(const uint16_t _value);
+        private:
+            Register8 high;
+            Register8 low;
+        };
+    }
+}
 
 #endif //FREEBOY_REGISTERS_H
--- freeboy/src/Registers.cpp
+++ freeboy/src/Registers.cpp
@@ -3,3 +3,140 @@
 //
 
 #include "../include/Registers.h"
+
+namespace gameboy
+{
+    namespace cpu_register
+    {
+
+        Register8::Register8(uint8_t _value) : value(_value) {}
+
+        uint8_t Register8::operator++(int)
+        {
+            uint8_t temp = read();
+            write(temp + 1);
+            return temp;
+        }
+
+        uint8_t Register8::operator++()
+        {
+            uint8_t temp = read() + 1;
+            write(temp);
+            return temp;
+        }
+
+        uint8_t Register8::operator--(int) {
+            uint8_t temp = read();
+            write(temp - 1);
+            return temp;        }
+
+        uint8_t Register8::operator--() {
+            uint8_t temp = read() - 1;
+            write(temp);
+            return temp;
+        }
+
+        void Register8::operator+=(const uint8_t _value)
+        {
+            uint8_t current = read();
+            write(current + _value);
+        }
+
+        void Register8::operator^=(const Register8 &_register)
+        {
+            value ^= _register.read();
+        }
+
+        void Register8::operator|=(const Register8& _register)
+        {
+            value |= _register.read();
+        }
+
+        void Register8::operator&=(const uint16_t _value)
+        {
+            value &= _value;
+        }
+
+        void Register8::operator=(const Register8 &_register)
+        {
+            value = _register.read();
+        }
+
+        void Register8::operator-=(const uint8_t _value)
+        {
+            value -= _value;
+        }
+
+        /* -------------------------------------------------------------------------- */
+
+        Register16::Register16(uint16_t _value) :
+            high{static_cast<uint8_t>(_value >> 8 & 0xFF)},
+            low{static_cast<uint8_t>(_value & 0xFF)} {}
+
+        void Register16::write(uint16_t _value)
+        {
+            uint8_t h = _value >> 8 & 0xFF;
+            uint8_t l = _value & 0xFF;
+
+            high.write(h);
+            low.write(l);
+        };
+
+        uint16_t Register16::operator++(int)
+        {
+            uint16_t temp = read();
+            write(temp + 1);
+            return temp;
+        }
+
+        uint16_t Register16::operator++()
+        {
+            uint16_t temp = read() + 1;
+            write(temp);
+            return temp;
+        }
+
+        void Register16::operator+=(const uint8_t _value)
+        {
+            uint16_t data = read() + _value;
+            write(data);
+        }
+
+        uint16_t Register16::operator--(int)
+        {
+            uint16_t temp = read();
+            write(temp - 1);
+            return temp;
+        }
+
+        uint16_t Register16::operator--()
+        {
+            uint16_t temp = read() - 1;
+            write(temp);
+            return temp;
+        }
+
+        void Register16::operator=(const Register16 &_register)
+        {
+            uint16_t _value = _register.read();
+            write(_value);
+        }
+
+        void Register16::operator=(const uint16_t _value)
+        {
+            write(_value);
+        }
+
+        void Register16::operator-=(const uint8_t _value)
+        {
+            uint16_t temp = read() - _value;
+            write(temp);
+        }
+
+        void Register16::operator+=(const Register16 _register)
+        {
+            uint16_t result = read() + _register.read();
+            write(result);
+        }
+    }
+}
