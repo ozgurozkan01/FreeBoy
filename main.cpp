0aca04a50192e294625b6563065e7169e6b5c268
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-06-22 01:28:06 +0300

    update main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "freeboy/include/GameBoy.h"
 
 int main(int argc, char **argv) {
-    gameboy::GameBoy gameBoy;
+    gameboy::GameBoy gameBoy("C:/Users/ozgur/GitHub/FreeBoy/ROMs/cpu_instrs.gb");
     if (gameBoy.init())
     {
         gameBoy.run();
