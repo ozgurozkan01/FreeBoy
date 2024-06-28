539128ebe4822da8decf99cc66f2d85a02b44832
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-06-28 22:19:17 +0300

    add interrupt handle and trigger implementation

--- freeboy/include/InterruptHandler.h
+++ freeboy/include/InterruptHandler.h
@@ -6,16 +6,22 @@
 #define FREEBOY_INTERRUPTHANDLER_H
 
 #include <cstdint>
+#include "Registers.h"
 
 namespace gameboy
 {
-    enum class InterruptFlags : uint8_t
+    class CPU;
+    class MMU;
+
+    using namespace cpu_register;
+
+    enum class InterruptType : uint8_t
     {
-        vblank = 0,
-        lcd = 1,
-        timer = 2,
-        serial = 3,
-        joypad = 4
+        vblank = 0x0,
+        lcd = 0x2,
+        timer = 0x4,
+        serial = 0x8,
+        joypad = 0x10
     };
 
     class InterruptHandler {
@@ -23,18 +29,25 @@ namespace gameboy
 
         InterruptHandler() : IME(false) {}
 
+        void handle(CPU* _cpu, MMU* _mmu);
+
         void setIME(const bool _isEnable) { IME = _isEnable; };
-        void setIE(const uint8_t _value) { IE = _value; };
-        void setIF(const uint8_t _value) { IF = _value; };
+        void setIE(const uint8_t _value)  { IE = _value; };
+        void setIF(const uint8_t _value)  { IF = _value; };
 
         bool getIME() const { return IME; }
-        uint8_t getIE() const { return IE; }
-        uint8_t getIF() const { return IF; }
+        Register8& getIE()  { return IE; }
+        Register8& getIF()  { return IF; }
 
     private:
+        void trigger(CPU* _cpu, MMU* _mmu, const uint16_t _address ,const InterruptType _type);
+
+        bool isEnableIF(InterruptType _type);
+        bool isEnableIE(InterruptType _type);
+
         bool IME;
-        uint8_t IE;
-        uint8_t IF;
+        Register8 IE; // 0xFFFF
+        Register8 IF; // 0xFF0F
     };
 }
 
---
+++ freeboy/src/InterruptHandler.cpp
@@ -0,0 +1,32 @@
+//
+// Created by ozgur on 6/28/2024.
+//
+
+#include "../include/InterruptHandler.h"
+#include "../include/CPU.h"
+#include "../include/MMU.h"
+
+namespace gameboy
+{
+
+    void InterruptHandler::handle(CPU* _cpu, MMU* _mmu)
+    {
+        if      (isEnableIF(InterruptType::vblank) && isEnableIE(InterruptType::vblank)) { trigger(_cpu, _mmu, 0x40, InterruptType::vblank); }
+        else if (isEnableIF(InterruptType::lcd)    && isEnableIE(InterruptType::lcd))    { trigger(_cpu, _mmu, 0x48, InterruptType::lcd);    }
+        else if (isEnableIF(InterruptType::timer)  && isEnableIE(InterruptType::timer))  { trigger(_cpu, _mmu, 0x50, InterruptType::timer);  }
+        else if (isEnableIF(InterruptType::serial) && isEnableIE(InterruptType::serial)) { trigger(_cpu, _mmu, 0x58, InterruptType::serial); }
+        else if (isEnableIF(InterruptType::joypad) && isEnableIE(InterruptType::joypad)) { trigger(_cpu, _mmu, 0x60, InterruptType::joypad); }
+    }
+
+    bool InterruptHandler::isEnableIF(InterruptType _type) { return IF & static_cast<uint8_t>(_type); }
+    bool InterruptHandler::isEnableIE(InterruptType _type) { return IE & static_cast<uint8_t>(_type); }
+
+    void InterruptHandler::trigger(CPU* _cpu, MMU* _mmu, const uint16_t _address ,const InterruptType _type)
+    {
+        _mmu->push(_cpu->SP, _cpu->PC);
+        _cpu->PC = _address;
+        _cpu->isHalted = false;
+        IF = ~(static_cast<uint8_t>(_type));
+        IME = false;
+    }
+}
