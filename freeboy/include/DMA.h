9913093186893904ad459ed538289e0cff441c77
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-07-03 23:49:18 +0300

    add direct access memory

---
+++ freeboy/include/DMA.h
@@ -0,0 +1,39 @@
+//
+// Created by ozgur on 6/30/2024.
+//
+
+#ifndef FREEBOY_DMA_H
+#define FREEBOY_DMA_H
+
+#include <cstdint>
+#include "PPU.h"
+
+namespace gameboy
+{
+    class MMU;
+
+    using namespace graphic;
+
+    /*
+     DMA transfer is used to quickly copy a block of data from a part of memory (ROM or RAM)
+     to another part of memory, specifically to the OAM.
+     This is important for updating the sprite attributes efficiently.
+    */
+    class DMA {
+    public:
+        DMA(PPU* _ppu, MMU* _mmu);
+
+        void startTransfer(uint8_t _address);
+        void tick();
+        bool isTransferring();
+    private:
+        bool isActive;
+        uint8_t lowerByte;
+        uint8_t upperByte;
+
+        PPU* ppuPtr;
+        MMU* mmuPtr;
+    };
+}
+
+#endif //FREEBOY_DMA_H
---
+++ freeboy/src/DMA.cpp
@@ -0,0 +1,39 @@
+//
+// Created by ozgur on 6/30/2024.
+//
+
+#include "../include/DMA.h"
+#include "../include/MMU.h"
+#include <chrono>
+#include <thread>
+
+namespace gameboy
+{
+    DMA::DMA(PPU *_ppu, MMU *_mmu) :
+        ppuPtr(_ppu),
+        mmuPtr(_mmu)
+    {}
+
+    void DMA::startTransfer(uint8_t _address)
+    {
+        isActive = true;
+        lowerByte = 0;
+        upperByte = _address;
+    }
+
+    void DMA::tick()
+    {
+        if (!isActive) { return; }
+
+        // Source:      $XX00-$XX9F
+        // Destination: $FE00-$FE9F
+        uint16_t address = 0xFE00 | lowerByte;
+        uint8_t data = mmuPtr->read8((upperByte * 0x100) + lowerByte);
+        ppuPtr->writeOAM(address, data);
+
+        // If lowerByte is bigger than 0x9F, that means invalid memory address for transfering
+        isActive = ++lowerByte < 0xA0;
+    }
+
+    bool DMA::isTransferring() { return isActive; }
+}
