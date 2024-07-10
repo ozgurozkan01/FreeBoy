5c98a5d651b53f3375a2f62a4ef49e8b61f0420c
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2024-07-10 23:18:20 +0300

    complet ppu implementation

--- freeboy/include/PPU.h
+++ freeboy/include/PPU.h
@@ -7,20 +7,53 @@
 
 #include <cstdint>
 #include <array>
+#include <queue>
 
 namespace gameboy
 {
+    class InterruptHandler;
+    class MMU;
+
+    /*
+     LCD viewport 20x18 tiles (160x144 pixels)
+
+     3 Layers : Background 32x32 Tiles (256x256 pixels)
+                Window     32x32 Tiles (256x256 pixels)
+                Sprites
+     */
+
     namespace graphic
     {
+        class LCD;
+
+        namespace ModeCycles
+        {
+            // Cycle Amounts of each mode in per scanline
+            const uint16_t hBlankCycle = 204;
+            const uint16_t vBlankCycle = 456;
+            const uint8_t oamCycle = 80;
+            const uint16_t transferCycle = 172;
+        }
 
         class PPU {
+            friend class LCD;
+            friend class Renderer;
+            // Fetcher stands for fetching tile data and preparing it for the Pixel FIFO
+            enum class FetcherState : uint8_t
+            {
+                getTile,
+                getDataLow,
+                getDataHigh,
+                sleep,
+                pushPixel
+            };
 
             enum class PPUMode : uint8_t
             {
-                hBlank,
-                vBlank,   // LY > 143
-                oamScan,
-                transferData
+                hBlank = 0, // 87–204 dots
+                vBlank = 1, // 4560 dots (10 scanlines = 144-153)
+                oamScan = 2, // 80 dots
+                transferData = 3 // 172–289 dots
             };
 
             // Sprite data is stored in the OAM section of memory which can fit up to 40 sprites.
@@ -29,11 +62,37 @@ namespace gameboy
                 uint8_t y;
                 uint8_t x;
                 uint8_t tileIndex;
-                uint8_t attributes;
-            } oamRAM[0x28] = {0};
+                struct Attributes
+                {
+                    uint8_t  cgbPalette : 3;
+                    uint8_t  vramBank : 1;
+                    uint8_t  dmgPalette : 1;
+                    uint8_t  xFlip : 1;
+                    uint8_t  yFlip : 1;
+                    uint8_t  priority : 1;
+                }attributes;
+            };
+
+            struct PixelFIFO
+            {
+                FetcherState currentState;
+                uint8_t scanlineX;
+                uint8_t pushedX;
+                uint8_t fetcherX;
+                uint8_t mapY;
+                uint8_t mapX;
+                uint8_t tileY;
+                uint8_t fifoX;
+                std::array<uint8_t, 3> bgwFetchData;
+                std::array<uint8_t, 6> spriteFetchData;
+                std::queue<uint32_t> fifo;
+
+            }pixelFifo;
 
         public:
-            PPU() = default;
+            PPU(InterruptHandler* _interruptHandler, MMU* _mmu);
+
+            void tick();
 
             void writeVRAM(const uint16_t _address, const uint8_t _value);
             uint8_t readVRAM(const uint16_t _address);
@@ -41,8 +100,70 @@ namespace gameboy
             void writeOAM(const uint16_t _address, const uint8_t _value);
             uint8_t readOAM(const uint16_t _address);
 
+            void connectLCD(LCD* _lcd) { lcdPtr = _lcd; setPPUMode(PPUMode::oamScan);};
+
+            uint32_t getRenderedFrame() const { return renderedFrame;}
+            void calculateFPS();
+
+            bool isFifoFull();
         private:
-            std::array<uint8_t, 0x2000> videoRAM = {0};
+            void setPPUMode(const PPUMode _mode);
+            PPUMode getPPUMode();
+
+            void runOAM();
+            void runHBlank();
+            void runVBlank();
+            void runTransferData();
+
+            void resetPixelFIFO();
+            void updateVisibleSprites();
+            void fetchSpriteTiles();
+            void fetchSpriteData(uint8_t _offset);
+            uint32_t fetchSpritePixels(uint8_t _bit, uint32_t _color, uint8_t _colorIndex);
+
+            void pipelineProcess();
+            void pipelineFetch();
+            void pipelinePushPixel();
+            void pipelineResetFIFO();
+            bool canPushToFIFO();
+
+            void runGetTileState();
+            void runGetDataLowState();
+            void runGetDataHightState();
+            void runSleepState();
+            void runPushPixelState();
+
+            void loadWindowTile();
+
+            const uint8_t maxSpritePerLine;
+            const uint8_t maxSpriteAmount;
+            const uint8_t fifoCapacity;
+            // Window
+            uint8_t visibleWindowCounter;
+            // Renderin Variables
+            const uint16_t visibleScanlinePerFrame;
+            const uint16_t scanlinePerFrame;
+            uint16_t scanlineTCycle;
+            // FPS variables...
+            const float targetFPS;
+            const float targetDeltaTime;
+            uint32_t frameDuration;
+            uint32_t previousFrame;
+            uint32_t calculationStartTime;
+            uint32_t frameCount;
+            uint32_t renderedFrame;
+
+
+            // Memory
+            std::array<uint8_t, 0x2000> videoRAM;
+            std::array<OAM, 0x28> oamRAM;
+            std::array<uint32_t, 160 * 144> videoBuffer; // Pixel data buffer for rendering
+            std::vector<OAM> lineSprites; // max 10
+            std::vector<OAM> fetchedLineSprites; // max 3
+            // Components
+            InterruptHandler* interruptHandlerPtr;
+            LCD* lcdPtr;
+            MMU* mmuPtr;
         };
     }
 }
--- freeboy/src/PPU.cpp
+++ freeboy/src/PPU.cpp
@@ -3,9 +3,48 @@
 //
 
 #include "../include/PPU.h"
+#include "../include/MMU.h"
+#include "../include/LCD.h"
+#include "../include/Renderer.h"
+#include "../include/InterruptHandler.h"
+#include <algorithm>
 
 namespace gameboy::graphic
 {
+
+    PPU::PPU(InterruptHandler* _interruptHandler, MMU* _mmu) :
+            // FPS
+            targetFPS(60),
+            targetDeltaTime(1000 / targetFPS),
+            calculationStartTime(0),
+            previousFrame(0),
+            frameCount(0),
+            renderedFrame(0),
+            frameDuration(0),
+            // Window
+            visibleWindowCounter(0),
+            // Informations of One Frame
+            visibleScanlinePerFrame(144),
+            scanlinePerFrame(154),
+            scanlineTCycle(0),
+            fifoCapacity(8),
+            maxSpritePerLine(10),
+            maxSpriteAmount(40),
+            // RAM
+            videoRAM{0},
+            oamRAM{OAM()},
+            videoBuffer{0},
+            // Components
+            interruptHandlerPtr(_interruptHandler),
+            mmuPtr(_mmu)
+            {
+        // Pixel FIFO
+        pixelFifo.scanlineX = 0;
+        pixelFifo.pushedX = 0;
+        pixelFifo.fetcherX = 0;
+        pixelFifo.currentState = FetcherState::getTile;
+    }
+
     void PPU::writeVRAM(const uint16_t _address, const uint8_t _value)
     {
         videoRAM[_address - 0x8000] = _value;
@@ -26,5 +65,416 @@ namespace gameboy::graphic
     {
         auto* p = reinterpret_cast<uint8_t*>(&oamRAM);
         return p[_address - 0xFE00];
+    }
+
+    void PPU::runHBlank()
+    {
+        if (scanlineTCycle >= ModeCycles::hBlankCycle)
+        {
+            lcdPtr->incrementLY();
+
+            if (lcdPtr->ly.read() < visibleScanlinePerFrame)
+            {
+                setPPUMode(PPUMode::oamScan);
+            }
+            else
+            {
+                setPPUMode(PPUMode::vBlank);
+                interruptHandlerPtr->requestInterrupt(InterruptType::vblank);
+
+                if (lcdPtr->isInterruptEnabled(LCD::Status::vBlankEnable))
+                {
+                    interruptHandlerPtr->requestInterrupt(InterruptType::lcd);
+                }
+
+                calculateFPS();
+            }
+
+            scanlineTCycle = 0;
+        }
+    }
+
+    void PPU::runVBlank()
+    {
+        if (scanlineTCycle >= ModeCycles::vBlankCycle)
+        {
+            lcdPtr->incrementLY();
+
+            if (lcdPtr->ly.read() >= scanlinePerFrame)
+            {
+                setPPUMode(PPUMode::oamScan);
+                lcdPtr->ly = 0;
+                visibleWindowCounter = 0;
+            }
+
+            scanlineTCycle = 0;
+        }
+    }
+
+    void PPU::runOAM()
+    {
+        if (scanlineTCycle >= ModeCycles::oamCycle)
+        {
+            setPPUMode(PPUMode::transferData);
+
+            resetPixelFIFO();
+        }
+
+        updateVisibleSprites();
+    }
+
+    void PPU::runTransferData()
+    {
+        pipelineProcess();
+
+        if (pixelFifo.pushedX >= Renderer::xResolution)
+        {
+            pipelineResetFIFO();
+            setPPUMode(PPUMode::hBlank);
+            if (lcdPtr->isInterruptEnabled(LCD::Status::hBlankEnable))
+            {
+                interruptHandlerPtr->requestInterrupt(InterruptType::lcd);
+            }
+        }
+    }
+
+    bool PPU::isFifoFull()
+    {
+        return pixelFifo.fifo.size() >= fifoCapacity;
+    }
+
+    void PPU::resetPixelFIFO()
+    {
+        pixelFifo.currentState = FetcherState::getTile;
+        pixelFifo.scanlineX = 0;
+        pixelFifo.fetcherX = 0;
+        pixelFifo.pushedX = 0;
+        pixelFifo.fifoX = 0;
+    }
+
+    void PPU::updateVisibleSprites()
+    {
+        if (scanlineTCycle != 1) { return; }
+
+        lineSprites.clear();
+
+        uint16_t currentLine = lcdPtr->ly.read();
+        uint8_t spriteHeight = lcdPtr->readLCDC(LCD::LCDCFlag::spriteSize);
+
+        lineSprites.clear();
+
+        for (int spriteNumber = 0; spriteNumber < maxSpriteAmount; ++spriteNumber)
+        {
+            if (!oamRAM[spriteNumber].x) { continue; }
+            if (lineSprites.size() >= maxSpritePerLine) { break; }
+
+            //printf("If VALUE : %d\n", oamRAM[spriteNumber].y <= currentLine + 16 && oamRAM[spriteNumber].y + spriteHeight > currentLine + 16);
+
+            if (oamRAM[spriteNumber].y <= currentLine + 16 && oamRAM[spriteNumber].y + spriteHeight > currentLine + 16)
+            {
+                auto it = std::upper_bound(
+                        lineSprites.begin(),
+                        lineSprites.end(),
+                        oamRAM[spriteNumber],
+                        [](const OAM& a, const OAM& b) { return a.x < b.x; }
+                );
+
+                lineSprites.insert(it, oamRAM[spriteNumber]);
+            }
+        }
+    }
+
+    void PPU::tick()
+    {
+        scanlineTCycle++;
+
+        switch (getPPUMode())
+        {
+            case PPUMode::hBlank: runHBlank(); break;
+            case PPUMode::vBlank: runVBlank(); break;
+            case PPUMode::oamScan: runOAM(); break;
+            case PPUMode::transferData: runTransferData(); break;
+        }
+    }
+
+
+    PPU::PPUMode PPU::getPPUMode()
+    {
+        return static_cast<PPUMode>(lcdPtr->lcds.read() & 0b11);
+    }
+
+    void PPU::setPPUMode(const PPU::PPUMode _mode)
+    {
+        lcdPtr->lcds &= ~(0b11); // Reset first 2 bit, if we did not reset, setting might be wrong.
+        lcdPtr->lcds |= static_cast<uint8_t>(_mode); // Set first 2 bit
+    }
+
+    void PPU::calculateFPS()
+    {
+        renderedFrame++;
+
+        uint32_t currentFrame = SDL_GetTicks();
+        frameDuration = currentFrame - previousFrame;
+
+        if (frameDuration < targetDeltaTime)
+        {
+            SDL_Delay(targetDeltaTime - frameDuration);
+        }
+
+        if (currentFrame - calculationStartTime >= 1000)
+        {
+            uint32_t fps = frameCount;
+            calculationStartTime = currentFrame;
+            frameCount = 0;
+            printf("FPS: %d\n", fps);
+        }
+
+        frameCount++;
+        previousFrame = SDL_GetTicks();
+    }
+
+    void PPU::pipelineProcess()
+    {
+        pixelFifo.mapY = lcdPtr->ly.read() + lcdPtr->scy.read();
+        pixelFifo.mapX = pixelFifo.fetcherX + lcdPtr->scx.read();
+        pixelFifo.tileY = ((lcdPtr->ly.read() + lcdPtr->scy.read()) % 8) * 2;
+
+        if (!(scanlineTCycle & 1))
+        {
+            pipelineFetch();
+        }
+
+        pipelinePushPixel();
+    }
+
+    void PPU::pipelinePushPixel()
+    {
+        if (isFifoFull())
+        {
+            uint32_t pixelData = pixelFifo.fifo.front();
+            pixelFifo.fifo.pop();
+            if (pixelFifo.scanlineX >= (lcdPtr->scx.read() % 8))
+            {
+                videoBuffer[pixelFifo.pushedX + (lcdPtr->ly.read() * Renderer::xResolution)] = pixelData;
+                pixelFifo.pushedX++;
+            }
+            pixelFifo.scanlineX++;
+        }
+    }
+
+    void PPU::pipelineFetch()
+    {
+        switch (pixelFifo.currentState)
+        {
+            case FetcherState::getTile:     runGetTileState(); break;
+            case FetcherState::getDataLow:  runGetDataLowState(); break;
+            case FetcherState::getDataHigh: runGetDataHightState(); break;
+            case FetcherState::sleep:       runSleepState(); break;
+            case FetcherState::pushPixel:   runPushPixelState(); break;
+        }
+    }
+
+    void PPU::runGetTileState()
+    {
+        fetchedLineSprites.clear();
+
+        if (lcdPtr->readLCDC(LCD::LCDCFlag::bgwEnable))
+        {
+            pixelFifo.bgwFetchData[0] = mmuPtr->read8(
+                    lcdPtr->readLCDC(LCD::LCDCFlag::bgTileMap) +
+                    ((pixelFifo.mapY / 8) * 32) +
+                    (pixelFifo.mapX / 8));
+
+
+            if (lcdPtr->readLCDC(LCD::LCDCFlag::bgwTileData) == 0x8800)
+            {
+                pixelFifo.bgwFetchData[0] += 128;
+            }
+
+            loadWindowTile();
+        }
+
+        if (lcdPtr->readLCDC(LCD::LCDCFlag::spriteEnable))
+        {
+            fetchSpriteTiles();
+        }
+
+        pixelFifo.currentState = FetcherState::getDataLow;
+        pixelFifo.fetcherX += 8;
+    }
+    void PPU::runGetDataLowState()
+    {
+        pixelFifo.bgwFetchData[1] = mmuPtr->read8(
+                lcdPtr->readLCDC(LCD::LCDCFlag::bgwTileData) +
+                (pixelFifo.bgwFetchData[0] * 16) +
+                pixelFifo.tileY
+                );
+
+        fetchSpriteData(0);
+
+        pixelFifo.currentState = FetcherState::getDataHigh;
+    }
+    void PPU::runGetDataHightState()
+    {
+        pixelFifo.bgwFetchData[2] = mmuPtr->read8(
+                lcdPtr->readLCDC(LCD::LCDCFlag::bgwTileData) +
+                (pixelFifo.bgwFetchData[0] * 16) +
+                pixelFifo.tileY + 1);
+
+        fetchSpriteData(1);
+
+        pixelFifo.currentState = FetcherState::sleep;
+    }
+    void PPU::runSleepState()
+    {
+        pixelFifo.currentState = FetcherState::pushPixel;
+    }
+    void PPU::runPushPixelState()
+    {
+        if (canPushToFIFO())
+        {
+            pixelFifo.currentState = FetcherState::getTile;
+        }
+    }
+
+    bool PPU::canPushToFIFO()
+    {
+        if (isFifoFull())
+        {
+            return false;
+        }
+
+        int x = pixelFifo.fetcherX - (8 - (lcdPtr->scx.read() % 8));
+        if (x < 0)
+        {
+            return true;
+        }
+
+        for (int bit = 7; bit >= 0; --bit)
+        {
+            uint8_t colorIndex = (!!(pixelFifo.bgwFetchData[2] & (1 << bit)) << 1) |
+                               (!!(pixelFifo.bgwFetchData[1] & (1 << bit)));
+
+            uint32_t color = static_cast<uint32_t>(lcdPtr->bgColors[colorIndex]);
+
+            if (!lcdPtr->readLCDC(LCD::LCDCFlag::bgwEnable))
+            {
+                colorIndex = static_cast<uint32_t>(lcdPtr->bgColors[0]);
+            }
+
+            if (lcdPtr->readLCDC(LCD::LCDCFlag::spriteEnable))
+            {
+                color = fetchSpritePixels(bit, color, colorIndex);
+            }
+
+            pixelFifo.fifo.push(color);
+            pixelFifo.fifoX++;
+        }
+
+        return true;
+    }
+
+
+    void PPU::pipelineResetFIFO()
+    {
+        while(!pixelFifo.fifo.empty())
+        {
+            pixelFifo.fifo.pop();
+        }
+    }
+
+    uint32_t PPU::fetchSpritePixels(uint8_t _bit, uint32_t _color, uint8_t _colorIndex)
+    {
+        for (int sprite = 0; sprite < fetchedLineSprites.size(); ++sprite)
+        {
+            int spriteX = (fetchedLineSprites.at(sprite).x - 8) + (lcdPtr->scx.read() % 8);
+            if (spriteX + 8 < pixelFifo.fifoX) { continue; }
+
+            int offset = pixelFifo.fifoX - spriteX;
+            if (offset < 0 || offset > 7) { continue; }
+
+            _bit = (7 - offset);
+
+            if (fetchedLineSprites.at(sprite).attributes.xFlip) { _bit = offset; }
+
+            uint8_t colorIndex = (!!(pixelFifo.spriteFetchData.at((sprite * 2) + 1) & (1 << _bit)) << 1) |
+                                 (!!(pixelFifo.spriteFetchData.at((sprite * 2))     & (1 << _bit)));
+            if (!colorIndex) { continue; }
+
+            bool bgPriority = fetchedLineSprites[sprite].attributes.priority;
+            if (!bgPriority || _colorIndex == 0)
+            {
+                _color = fetchedLineSprites[sprite].attributes.dmgPalette ?
+                        static_cast<uint32_t>(lcdPtr->spriteColor1[colorIndex]) : static_cast<uint32_t >(lcdPtr->spriteColor0[colorIndex]);
+
+                break;
+            }
+        }
+
+        return _color;
+    }
+
+    void PPU::fetchSpriteTiles()
+    {
+        for (auto &sprite : lineSprites)
+        {
+            if (fetchedLineSprites.size() >= 3) { break; }
+            int spriteX = (sprite.x - 8) + (lcdPtr->scx.read() % 8);
+
+            if ((spriteX >= pixelFifo.fetcherX && spriteX < pixelFifo.fetcherX + 8) ||
+                ((spriteX + 8) >= pixelFifo.fetcherX && (spriteX + 8) < pixelFifo.fetcherX + 8))
+            {
+                fetchedLineSprites.push_back(sprite);
+            }
+        }
+    }
+
+    void PPU::fetchSpriteData(uint8_t _offset)
+    {
+        uint16_t currentLine = lcdPtr->ly.read();
+        uint8_t spriteHeight = lcdPtr->readLCDC(LCD::LCDCFlag::spriteSize);
+
+        for (int sprite = 0; sprite < fetchedLineSprites.size(); ++sprite)
+        {
+            uint8_t tileY = ((currentLine + 16) - fetchedLineSprites.at(sprite).y) * 2;
+            //uint8_t tileY = ((currentLine + 16) - (fetchedLineSprites.at(sprite).y - 16)) * 2;
+
+            if (fetchedLineSprites.at(sprite).attributes.yFlip)
+            {
+                tileY = ((spriteHeight * 2) - 2) - tileY;
+            }
+
+            uint8_t tileIndex = fetchedLineSprites.at(sprite).tileIndex;
+
+            if (spriteHeight == 16)
+            {
+                tileIndex &= ~(1);
+            }
+
+            pixelFifo.spriteFetchData[(sprite * 2) + _offset] = mmuPtr->read8(0x8000 + (tileIndex * 16) + tileY + _offset);
+        }
+    }
+
+    void PPU::loadWindowTile()
+    {
+        if (!lcdPtr->isWindowVisible()) { return; }
+
+        if (pixelFifo.fetcherX + 7 >= lcdPtr->windowX.read() &&
+                pixelFifo.fetcherX + 7 < lcdPtr->windowX.read() + Renderer::yResolution + 14)
+        {
+            if (lcdPtr->ly >= lcdPtr->windowY && lcdPtr->ly < lcdPtr->windowY + Renderer::xResolution)
+            {
+                uint8_t windowTileY = visibleWindowCounter / 8;
+                pixelFifo.bgwFetchData[0] = mmuPtr->read8(
+                        lcdPtr->readLCDC(LCD::LCDCFlag::windowTileMap) +
+                        ((pixelFifo.fetcherX + 7 - lcdPtr->windowX.read()) / 8) +
+                        (windowTileY * 32));
+
+                if (lcdPtr->readLCDC(LCD::LCDCFlag::bgwTileData) == 0x8800)
+                {
+                    pixelFifo.bgwFetchData[0] += 128;
+                }
+            }
+        }
     }
 }
